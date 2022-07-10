ed9009665c5e5dc90b1bb9dfc2f63f00fcb1de96
pcleon <wpctszz@qq.com>
2022-07-10 08:40:00 +0800

    优化ha vip显示tag

--- src/components/Ha/index.vue
+++ src/components/Ha/index.vue
@@ -1,39 +1,39 @@
 <template>
   <el-table :data="list" border fit highlight-current-row style="width: 100%">
-
     <el-table-column
       v-loading="loading"
       align="center"
+      width="200px"
       label="实例"
       element-loading-text="耐心等待"
     >
-      <template slot-scope="scope">
-        <span>{{ scope.row.db_name }}</span>
+      <template slot-scope="{row}">
+        <span>{{ row.db_name }}</span>
       </template>
     </el-table-column>
 
     <el-table-column width="180px" align="center" label="主库">
-      <template slot-scope="scope">
-        <span>{{ scope.row.master }}</span>
+      <template slot-scope="{row}">
+        <span>{{ row.master }}</span>
       </template>
     </el-table-column>
 
     <el-table-column width="180px" align="center" label="备库">
-      <template slot-scope="scope">
-        <span>{{ scope.row.slave }}</span>
+      <template slot-scope="{row}">
+        <span>{{ row.slave }}</span>
       </template>
     </el-table-column>
 
-    <el-table-column width="200px" align="center" label="vip">
-      <template slot-scope="scope">
-        <span>{{ scope.row.vip }}</span>
-        <el-tag>{{ scope.row.v_tag }}</el-tag>
+    <el-table-column width="220px" align="center" label="vip">
+      <template slot-scope="{row}">
+        <span>{{ row.vip }} </span>
+        <el-tag :type="row.v_tag | vTagFilter">{{ row.v_tag }}</el-tag>
       </template>
     </el-table-column>
 
     <el-table-column width="180px" align="center" label="仲裁">
-      <template slot-scope="scope">
-        <span>{{ scope.row.arbit }}</span>
+      <template slot-scope="{row}">
+        <span>{{ row.arbit }}</span>
       </template>
     </el-table-column>
   </el-table>
@@ -44,18 +44,18 @@ import { fetchHaList } from '@/api/ha'
 
 export default {
   filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'info',
-        deleted: 'danger'
+    vTagFilter(v_tag) {
+      const tagMap = {
+        master: '',
+        slave: 'danger'
       }
-      return statusMap[status]
+      return tagMap[v_tag]
     }
   },
   props: {
     arbit: {
-      type: String
+      type: String,
+      default: ''
     }
   },
   data() {
